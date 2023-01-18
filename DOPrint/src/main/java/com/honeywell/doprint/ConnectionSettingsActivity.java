cb16fda8429077e88f8f4c247f87838646399a71
Yohanes Rizky Gumilir <yohanes.rizky@ai.astra.co.id>
2023-01-18 14:37:01 +0700

    doprint

--- DOPrint/src/main/java/com/honeywell/doprint/ConnectionSettingsActivity.java
+++ DOPrint/src/main/java/com/honeywell/doprint/ConnectionSettingsActivity.java
@@ -5,7 +5,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
